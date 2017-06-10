ac97c8c14c9ece603f08c4e6921ccb94083f648e
ManuChandran <manuchandran@ufl.edu>
2017-06-09 22:14:43 -0400

    Testing circle drawing

--- SimpleSliderPlotGUI/SliderPlot_test3.py
+++ SimpleSliderPlotGUI/SliderPlot_test3.py
@@ -49,29 +49,25 @@ class E(tk.Tk):
         yvar    = [y*self.var.get() for y in range(len(y))]
         
         self.ax1 = self.fig.add_subplot(221)
-        self.ax2 = self.fig.add_subplot(222)
+        self.ax2 = self.fig.add_subplot(222,aspect='equal')
         #ax1.plot(x, yvar,'x')
         self.ax1.plot(1, int(self.var.get()) ,'x')
-        self.ax2.plot(int(self.var.get()),1 ,'o')
+        #self.ax2.plot(int(self.var.get()),1 ,'o')
+        circle2 = plt.Circle((0.5, 0.5), 0.2, color='blue')
+        self.ax2.add_artist(circle2)
 
-    
-#     prop = 10
-#        u = np.linspace(0, 2 * np.pi, 100)
-#        v = np.linspace(0, np.pi, 100)
-#        x = prop * np.outer(np.cos(u), np.sin(v))
-#        y = prop * np.outer(np.sin(u), np.sin(v))
-#        z = prop * np.outer(np.ones(np.size(u)), np.cos(v))
-
-       # t = ax.plot_surface(x, y, z,  rstride=4, cstride=4,color='lightgreen',linewidth=0)
-
+        
         self.exitbtn = tk.Button(self, text='Exit', command=self.destroy)
         self.exitbtn.pack(ipadx=0,side='top',anchor = 'ne')
+        
+
         
         self.btn = tk.Button(self,text='Reset All inputs',command=self.alt)
         self.btn.pack(ipadx=250)
         
         self.scale1 = tk.Scale(self, from_=1, to=10, orient = "horizontal", variable = self.var, command=self.myupdateScale, label = "Label1")
         self.scale1.pack(ipadx=500,anchor = 'ne')
+
         
         self.frame = tk.Frame(self)
         self.frame.pack(ipadx=500,ipady=100,fill='both', expand=1)
@@ -80,9 +76,12 @@ class E(tk.Tk):
         self.canvas.get_tk_widget().pack(side='top', fill='both')
         self.canvas._tkcanvas.pack(side='top', fill='both', expand=1)
         self.canvas.draw()
-
+        
+        self.toolbar = NavigationToolbar2TkAgg( self.canvas, self )
+        self.toolbar.update()
+        self.toolbar.pack()
+        
         self.label1text = tk.StringVar()
-        self.label1text.set("My Scale value = " + str(self.var.get()) )
         self.Label1 = tk.Label(self, textvariable = self.label1text) 
         self.Label1.pack(ipadx=10, anchor = 'nw')
 
@@ -96,7 +95,9 @@ class E(tk.Tk):
         self.ax1.clear()
         self.ax2.clear()
         self.ax1.plot(1, int(self.var.get()) ,'x')
-        self.ax2.plot(int(self.var.get()),1 ,'o')
+        #self.ax2.plot(int(self.var.get()),1 ,'o')
+        circle2 = plt.Circle((0.5, 0.5), self.var.get()/10, color='blue',)
+        self.ax2.add_artist(circle2)
         self.label1text.set("My Scale value = " + str(self.var.get()) )
         self.canvas.show()
         
