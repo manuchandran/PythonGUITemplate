cd071efc2fd3ff29ef924b7145426327999ce942
ManuChandran <manuchandran@ufl.edu>
2017-06-11 21:05:46 -0400

    Reset added

--- SimpleSliderPlotGUI/SliderPlot_test3.py
+++ SimpleSliderPlotGUI/SliderPlot_test3.py
@@ -40,32 +40,47 @@ class E(tk.Tk):
 
     def main(self):
         #self.fig = plt.figure()
-        self.fig = plt.figure() #figsize=(3.5,3.5))
+        self.initRadius         = 1
+        self.initSpeed          = 3000
+        self.initInterFrameDist = 4
+        self.initSpacing        = 6
+        self.initReserved       = 1
+        self.initZrail          = 1
+        self.StartS  = 1000;
+        self.EndS    = self.StartS + 25*25.6;
         
+        self.fig = plt.figure() #figsize=(3.5,3.5))
         #ax = Axes3D(self.fig)
-        self.var     = tk.IntVar()
-        x       = np.arange(0, 10, 0.2)
-        y       = np.sin(x)
-        yvar    = [y*self.var.get() for y in range(len(y))]
+        self.Radius    = tk.IntVar()
+        self.Radius.set(self.initRadius)
+        self.Speed     = tk.IntVar()
+        self.Speed.set(self.initSpeed)
+        self.InterFrameDist     = tk.IntVar()
+        self.InterFrameDist.set(self.initInterFrameDist)
+        self.Spacing            = tk.IntVar()
+        self.Spacing.set(self.initSpacing)
+        self.Reserved           = tk.IntVar()
+        self.Reserved.set(self.initReserved)
+        
+        #x       = np.arange(0, 10, 0.2)
+        #y       = np.sin(x)
+        #yvar    = [y*self.Radius.get() for y in range(len(y))]
         
         self.ax1 = self.fig.add_subplot(221)
         self.ax2 = self.fig.add_subplot(222,aspect='equal')
         #ax1.plot(x, yvar,'x')
-        self.ax1.plot(1, int(self.var.get()) ,'x')
-        #self.ax2.plot(int(self.var.get()),1 ,'o')
+        self.ax1.plot(1, int(self.Radius.get()) ,'x')
+        #self.ax2.plot(int(self.Radius.get()),1 ,'o')
         circle2 = plt.Circle((0.5, 0.5), 0.2, color='blue')
         self.ax2.add_artist(circle2)
 
-        
         self.exitbtn = tk.Button(self, text='Exit', command=self.destroy)
         self.exitbtn.pack(ipadx=0,side='top',anchor = 'ne')
         
-
-        
-        self.btn = tk.Button(self,text='Reset All inputs',command=self.alt)
+        self.btn = tk.Button(self,text='Reset All inputs',command=self.resetvalues)
         self.btn.pack(ipadx=250)
         
-        self.scale1 = tk.Scale(self, from_=1, to=10, orient = "horizontal", variable = self.var, command=self.myupdateScale, label = "Label1")
+        self.scale1 = tk.Scale(self, from_=1, to=10, orient = "horizontal", variable = self.Radius, command=self.myupdateScale, label = "Radius")
         self.scale1.pack(ipadx=500,anchor = 'ne')
 
         
@@ -85,8 +100,15 @@ class E(tk.Tk):
         self.Label1 = tk.Label(self, textvariable = self.label1text) 
         self.Label1.pack(ipadx=10, anchor = 'nw')
 
-    def alt(self):
-         print('alt function called')
+    def resetvalues(self):
+        print('Reset function called')
+        self.Radius.set(self.initRadius)
+        self.Speed.set(self.initSpeed)
+        self.InterFrameDist.set(self.initInterFrameDist)
+        self.Spacing.set(self.initSpacing)
+        self.Reserved.set(self.initReserved)
+        self.myupdateScale(1) # SEND A RANDOM NUMBER 
+        
     def dest(self):
         self.destroy()
         sys.exit()
@@ -94,15 +116,15 @@ class E(tk.Tk):
         print('updating scale value')
         self.ax1.clear()
         self.ax2.clear()
-        self.ax1.plot(1, int(self.var.get()) ,'x')
-        #self.ax2.plot(int(self.var.get()),1 ,'o')
-        circle2 = plt.Circle((0.5, 0.5), self.var.get()/10, color='blue',)
+        self.ax1.plot(1, int(self.Radius.get()) ,'x')
+        #self.ax2.plot(int(self.Radius.get()),1 ,'o')
+        circle2 = plt.Circle((0.5, 0.5), self.Radius.get()/10, color='blue',)
         self.ax2.add_artist(circle2)
-        self.label1text.set("My Scale value = " + str(self.var.get()) )
+        self.label1text.set("My Scale value = " + str(self.Radius.get()) )
         self.canvas.show()
-        
+            
 
 if __name__ == "__main__":
     app = E(None)
-    app.title('Embedding in TK')
+    app.title('Sample GUI')
     app.mainloop()
